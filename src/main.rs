ba5b5c9f6f2ae083f46d94af8df48c7cd4e1c987
Klemen Javoršek <klemen.javorsek@guest.arnes.si>
2025-05-19 16:01:23 +0200

    start writing a tokenizer for an expression evaluator

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,119 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+use std::{io};
+
+#[derive(Debug, PartialEq)]
+enum Operator {
+    Add,
+    Sub,
+    Mult,
+    Div,
+    Lpar,
+    Rpar,
+    Mystery
+}
+
+impl Operator {
+    fn precedence(&self) -> Option<i32>{
+        match self {
+            Operator::Add | Operator::Sub => Some(0),
+            Operator::Mult | Operator::Div => Some(1),
+            _ => None
+        }
+    }
+}
+
+#[derive(PartialEq)]
+enum State {
+    ReadNumber,
+    Operator(Operator),
+}
+
 
 fn main() {
-    println!("Guess the number!");
+    println!("Please input the expression you want to calculate the result of:)");
 
     loop {
-        let secret_number = rand::thread_rng().gen_range(1..=100);
+        println!("Please input your expression:");
 
-        println!("The secret number is: {secret_number}");
-
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
+        let mut expr = String::new();
 
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut expr)
             .expect("Failed to read line");
 
-        println!("You guessed: {guess}");
+        let mut num_buf = String::new();
+        let mut num_stack: Vec<i32> = Vec::new();
+        let mut op_stack:Vec<Operator> = Vec::new();
+        let mut state = State::ReadNumber;
+        let operators = "+-*/()";
+        let mut current_op:Operator;
+        for c in expr.chars(){
+            if c.is_digit(10) {
+                num_buf.push(c);
+                state = State::ReadNumber;
+            }
+            else if operators.contains(c){
+                state = match c {
+                        '+' => State::Operator(Operator::Add),
+                        '-' => State::Operator(Operator::Sub),
+                        '*' => State::Operator(Operator::Mult),
+                        '/' => State::Operator(Operator::Div),
+                        '(' => State::Operator(Operator::Lpar),
+                        ')' => State::Operator(Operator::Rpar),
+                        _ =>   State::Operator(Operator::Mystery),
+                    };
+                //this means we just stopped reading a number, so we
+                //have to process the number and put it into the num_stack
+                //one exception is a Lpar, so we have to check if there is
+                //anything to put in
+                if !num_buf.is_empty(){
+                        num_stack.push(num_buf.parse::<i32>().unwrap());
+                        num_buf.clear();
+                    }
+                current_op = match state {
+                    State::Operator(sort) => sort,
+                    _ => panic!(),
+                };
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+                while op_stack.is_empty() ||
+                        *op_stack.last().unwrap() == Operator::Lpar ||
+                        current_op.precedence().unwrap() == op_stack.last().unwrap().precedence().unwrap()
+                    {
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!")
-            },
+                    }
+            }
+            else{
+            }
+            /* match c {
+                content if content.is_digit(10) => num_buf.push(content),
+                '+' | '-' | '*' | '/' | '(' | ')' => {op_stack.push(match c {
+                        '+' => Operator::Add,
+                        '-' => Operator::Sub,
+                        '*' => Operator::Mult,
+                        '/' => Operator::Div,
+                        '(' => Operator::Lpar,
+                        ')' => Operator::Rpar,
+                        _ => Operator::Mystery
+                    });
+                    if !num_buf.is_empty(){
+                        num_stack.push(num_buf.parse::<i32>().unwrap());
+                        num_buf.clear();
+                    }
+                },
+                _ => {
+                    if !num_buf.is_empty(){
+                        num_stack.push(num_buf.parse::<i32>().unwrap());
+                        num_buf.clear();
+                    }
+                }
+            } */
         }
+        for num in num_stack{
+            print!("{num} ")
+        }
+        print!("\n");
+        for op in op_stack{
+            print! ("{:?} ", op);
+        }
+        print!("\n");
     }
 }
